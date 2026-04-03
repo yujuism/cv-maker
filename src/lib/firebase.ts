433b8006ab4088fd5131a11a3543e79f2ee3fd3a
Ryan <yujuism@gmail.com>
2026-04-03 10:30:55 +0700

    fix: switch to new cv-maker-6e43f firebase project

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,14 +5,14 @@ import { getStorage } from 'firebase/storage';
 import { getAnalytics, isSupported } from 'firebase/analytics';
 
 const firebaseConfig = {
-	apiKey: 'AIzaSyD06ExuuUkhznZz8JQkUIIKFOltf2BWAWM',
-	authDomain: 'cloudcomputing-72286.firebaseapp.com',
-	databaseURL: 'https://cloudcomputing-72286-default-rtdb.asia-southeast1.firebasedatabase.app',
-	projectId: 'cloudcomputing-72286',
-	storageBucket: 'cloudcomputing-72286.firebasestorage.app',
-	messagingSenderId: '388465197669',
-	appId: '1:388465197669:web:fdd6c1e5d02a4e91e8fc8c',
-	measurementId: 'G-K1NY2X11P7'
+	apiKey: 'AIzaSyDrSK-8ZRzBRvDWqiE5cBOhBDAYrXnh_H0',
+	authDomain: 'cv-maker-6e43f.firebaseapp.com',
+	databaseURL: 'https://cv-maker-6e43f-default-rtdb.asia-southeast1.firebasedatabase.app',
+	projectId: 'cv-maker-6e43f',
+	storageBucket: 'cv-maker-6e43f.firebasestorage.app',
+	messagingSenderId: '875923550392',
+	appId: '1:875923550392:web:d105bc74c0290a4e12044d',
+	measurementId: 'G-1EZBRX30F5'
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
